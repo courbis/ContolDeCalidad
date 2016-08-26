b552adcbe81b0b32f24a2394806e63186e969087
courbis <ju.courbis@gmail.com>
2016-08-26 17:00:07 -0300

    modelo Usuario

--- app/src/main/java/com/example/courbis/contoldecalidad/Modelo/modeloUsuario.java
+++ app/src/main/java/com/example/courbis/contoldecalidad/Modelo/modeloUsuario.java
@@ -4,4 +4,50 @@ package com.example.courbis.contoldecalidad.Modelo;
  * Created by courbis on 24-08-16.
  */
 public class modeloUsuario {
+
+    String nombre;
+    String apellidoP;
+    String apellidoM;
+    String rut;
+    String pass;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }

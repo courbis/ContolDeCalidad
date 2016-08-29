2d78f640c4e1cdcd406927319a53e9ac9a881abf
courbis <ju.courbis@gmail.com>
2016-08-28 22:28:31 -0300

    modelo Usuario

--- app/src/main/java/com/example/courbis/contoldecalidad/Controlador.java
+++ app/src/main/java/com/example/courbis/contoldecalidad/Controlador.java
@@ -1,13 +1,77 @@
 package com.example.courbis.contoldecalidad;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Controlador extends AppCompatActivity {
+import com.example.courbis.contoldecalidad.DB.AdminSQLiteOpenHelper;
+import com.example.courbis.contoldecalidad.Modelo.modeloUsuario;
+
+public class Controlador extends AppCompatActivity /*implements View.OnClickListener*/ {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vista_login);
+        setContentView(R.layout.vista_formulario);
     }
+
+/*
+    @Override
+    public void onClick(View v) {
+        Log.d("pruebaLogin","estoy aca");
+//        llenaBase();
+        //Login();
+    }
+
+    public void llenaBase(){
+
+        AdminSQLiteOpenHelper adminSQLiteOpenHelper= new AdminSQLiteOpenHelper(this, "controlDeCalidad", null, 1);
+        SQLiteDatabase dbw= adminSQLiteOpenHelper.getWritableDatabase();
+
+        ContentValues registro= new ContentValues();
+        registro.put("rut","1234567");
+        registro.put("nombre","Julio");
+        registro.put("apellido_paterno","Courbis");
+        registro.put("apellido_materno","Palma");
+        registro.put("pass","1234567");
+        dbw.insert("usuario", null, registro);
+
+
+
+    }
+
+    public void Login(){
+
+        modeloUsuario mUsuario=new modeloUsuario();
+        mUsuario.setRut(edtUsuario.getText().toString());
+        mUsuario.setPass(edtPass.getText().toString());
+        String compruebaPass = null;
+
+
+        AdminSQLiteOpenHelper adminSQLiteOpenHelper= new AdminSQLiteOpenHelper(this, "controlDeCalidad", null, 1);
+        SQLiteDatabase dbw= adminSQLiteOpenHelper.getWritableDatabase();
+        Cursor fila= dbw.rawQuery("select pass from usuario where rut="+mUsuario.getRut(), null);
+        if(fila.moveToFirst()){
+            compruebaPass=fila.getString(0);
+
+        }else{
+            Toast.makeText(this,"usuario inexistente", Toast.LENGTH_SHORT).show();
+
+        }
+        dbw.close();
+
+
+        if(mUsuario.getPass().equals(compruebaPass)){
+            setContentView(R.layout.vista_tarea);
+        }
+    }
+
+*/
 }
